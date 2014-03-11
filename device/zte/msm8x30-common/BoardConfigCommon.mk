dd0d3ff30210080ccbf2a840c1bcc1d83cb4062d
hroark13 <hroark13@dev01.(none)>
2014-03-11 08:26:16 -0400

    clean up board file

--- device/zte/msm8x30-common/BoardConfigCommon.mk
+++ device/zte/msm8x30-common/BoardConfigCommon.mk
@@ -23,93 +23,54 @@ BOARD_VENDOR := ZTE
 
 # Bootloader
 TARGET_NO_BOOTLOADER := true
+#TARGET_BOOTLOADER_BOARD_NAME := MSM8960
 
-# Architecture
-TARGET_ARCH := arm
-TARGET_CPU_ABI := armeabi-v7a
-TARGET_CPU_ABI2 := armeabi
-TARGET_ARCH_VARIANT := armv7-a-neon
-
-# PowerHAL
-TARGET_USES_CM_POWERHAL := true
-
-# chargers
-BOARD_CHARGER_RES := device/zte/msm8x30-common/ramdisk/res/images/charger
-
-# Recovery
-TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
-
-# QCOM hardware
-TARGET_QCOM_AUDIO_VARIANT := caf
-TARGET_QCOM_DISPLAY_VARIANT := caf
-BOARD_USES_LEGACY_ALSA_AUDIO := true
-
-# Graphics
-USE_OPENGL_RENDERER := true
-TARGET_USES_C2D_COMPOSITION := true
-TARGET_USES_ION := true
-
-# Charging mode
-BOARD_CHARGING_MODE_BOOTING_LPM := /sys/class/power_supply/battery/batt_lp_charging
-BOARD_BATTERY_DEVICE_NAME := "battery"
-
-#TARGET_RELEASETOOLS_EXTENSIONS := device/zte/msm8x30-common
-
-#BOARD_HARDWARE_CLASS := hardware/zte/cmhw
-
-# Override healthd HAL
-#BOARD_HAL_STATIC_LIBRARIES := libhealthd.qcom
-
+# Kernel
+BOARD_KERNEL_CMDLINE         := androidboot.hardware=qcom androidboot.selinux=permissive user_debug=31 zcache
+BOARD_KERNEL_BASE            := 0x80200000
+BOARD_MKBOOTIMG_ARGS         := --ramdisk_offset 0x02000000
+BOARD_KERNEL_PAGESIZE        := 2048
+TARGET_KERNEL_SOURCE         := kernel/zte/8x30hK
+TARGET_KERNEL_SELINUX_CONFIG := selinux_defconfig
+TARGET_SPECIFIC_HEADER_PATH := device/zte/msm8x30-common/include
 
 # Platform
 TARGET_BOARD_PLATFORM := msm8960
 TARGET_CPU_VARIANT := krait
 
 # Architecture
+TARGET_ARCH := arm
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_SMP := true
 
 # Flags for Krait CPU
 TARGET_GLOBAL_CFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
 TARGET_GLOBAL_CPPFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
 
-# Adreno configuration
+# Graphics
+TARGET_QCOM_DISPLAY_VARIANT := caf
+USE_OPENGL_RENDERER := true
+TARGET_USES_C2D_COMPOSITION := true
+TARGET_USES_ION := true
 BOARD_EGL_CFG := device/zte/msm8x30-common/proprietary/lib/egl/egl.cfg
-
-# Wifi driver
-BOARD_HAS_QCOM_WLAN              := true
-BOARD_HAS_QCOM_WLAN_SDK          := true
-BOARD_WLAN_DEVICE                := qcwcn
-WPA_SUPPLICANT_VERSION           := VER_0_8_X
-BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
-BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
-BOARD_HOSTAPD_DRIVER             := NL80211
-BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
-WIFI_DRIVER_FW_PATH_STA          := "sta"
-WIFI_DRIVER_FW_PATH_AP           := "ap"
-WIFI_DRIVER_MODULE_PATH := "/system/lib/modules/wlan.ko"
-WIFI_DRIVER_MODULE_NAME := "wlan"
 
 # QCOM
 BOARD_USES_QCOM_HARDWARE := true
 TARGET_USES_QCOM_BSP := true
 COMMON_GLOBAL_CFLAGS += -DQCOM_HARDWARE -DQCOM_BSP
 
-# Bluetooth
-BOARD_HAVE_BLUETOOTH := true
-BOARD_HAVE_BLUETOOTH_QCOM := true
-
-# Vold
-BOARD_VOLD_MAX_PARTITIONS := 24
-TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun%d/file
-
-# Workaround to avoid issues with legacy liblights on QCOM platforms
-TARGET_PROVIDES_LIBLIGHT := true
-
 # Audio
+TARGET_QCOM_AUDIO_VARIANT := caf
+BOARD_USES_LEGACY_ALSA_AUDIO := true
 BOARD_USES_ALSA_AUDIO := true
 BOARD_USES_FLUENCE_INCALL := true
 BOARD_USES_SEPERATED_AUDIO_INPUT := true
 COMMON_GLOBAL_CFLAGS += -DLPA_DEFAULT_BUFFER_SIZE=32
+BOARD_USES_SEPERATED_VOICE_SPEAKER := true
+BOARD_USES_SEPERATED_VOIP := true
+BOARD_HAVE_DOCK_USBAUDIO := true
 
 # QCOM enhanced A/V
 TARGET_ENABLE_QC_AV_ENHANCEMENTS := true
@@ -125,21 +86,59 @@ USE_CAMERA_STUB := false
 TARGET_PROVIDES_CAMERA_HAL := true
 USE_DEVICE_SPECIFIC_CAMERA := true
 COMMON_GLOBAL_CFLAGS += -DSAMSUNG_CAMERA_HARDWARE
+TARGET_NEED_CAMERA_ZSL := true
+TARGET_NEED_FFC_PICTURE_FIXUP := true
 
-TARGET_SPECIFIC_HEADER_PATH := device/zte/msm8x30-common/include
+# Wifi driver
+BOARD_HAS_QCOM_WLAN              := true
+BOARD_HAS_QCOM_WLAN_SDK          := true
+BOARD_WLAN_DEVICE                := qcwcn
+WPA_SUPPLICANT_VERSION           := VER_0_8_X
+BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
+BOARD_HOSTAPD_DRIVER             := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
+WIFI_DRIVER_FW_PATH_STA          := "sta"
+WIFI_DRIVER_FW_PATH_AP           := "ap"
+WIFI_DRIVER_MODULE_PATH := "/system/lib/modules/wlan.ko"
+WIFI_DRIVER_MODULE_NAME := "wlan"
+
+# Bluetooth
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_QCOM := true
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/zte/msm8x30-common/bluetooth
+
+# We have new GPS
+BOARD_HAVE_NEW_QC_GPS := true
+
+# Enable QCOM FM feature
+QCOM_FM_ENABLED := true
+BOARD_USES_SEPERATED_FM := true
+
+# Time services
+BOARD_USES_QC_TIME_SERVICES := true
+
+# PowerHAL
+TARGET_USES_CM_POWERHAL := true
+
+# Charger
+BOARD_CHARGING_MODE_BOOTING_LPM := /sys/class/power_supply/battery/batt_lp_charging
+BOARD_BATTERY_DEVICE_NAME := "battery"
+BOARD_CHARGER_ENABLE_SUSPEND := true
+BOARD_CHARGER_RES := device/zte/msm8x30-common/ramdisk/res/images/charger
+
+# Vold
+BOARD_VOLD_MAX_PARTITIONS := 24
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun%d/file
+
+# Workaround to avoid issues with legacy liblights on QCOM platforms
+TARGET_PROVIDES_LIBLIGHT := true
 
 # For backwards compatibility with camera blobs
 COMMON_GLOBAL_CFLAGS += -DNEEDS_VECTORIMPL_SYMBOLS
 
-# Kernel
-BOARD_KERNEL_CMDLINE         := androidboot.hardware=qcom androidboot.selinux=permissive user_debug=31 zcache
-BOARD_KERNEL_BASE            := 0x80200000
-BOARD_MKBOOTIMG_ARGS         := --ramdisk_offset 0x02000000
-BOARD_KERNEL_PAGESIZE        := 2048
-TARGET_KERNEL_SOURCE         := kernel/zte/8x30hK
-TARGET_KERNEL_SELINUX_CONFIG := selinux_defconfig
-
-TARGET_BOOTLOADER_BOARD_NAME := MSM8960
+# Disable initlogo, ZTE framebuffer is weird
+TARGET_NO_INITLOGO := true
 
 # Recovery
 BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/zte/msm8x30-common/recovery/recovery_keys.c
@@ -149,44 +148,12 @@ TARGET_RECOVERY_FSTAB := device/zte/msm8x30-common/ramdisk/fstab.qcom
 TARGET_RECOVERY_INITRC := device/zte/msm8x30-common/recovery/root/init.recovery.rc
 TARGET_RECOVERY_LCD_BACKLIGHT_PATH := \"/sys/class/lcd/panel/backlight\"
 COMMON_GLOBAL_CFLAGS += -DNO_SECURE_DISCARD
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 
 TARGET_USERIMAGES_USE_EXT4 := true
-BOARD_BOOTIMAGE_PARTITION_SIZE := 10485760
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 10485760
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1572864000
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 5821676544
-BOARD_CACHEIMAGE_PARTITION_SIZE := 209715200
+BOARD_BOOTIMAGE_PARTITION_SIZE := 16777216
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16777216
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1082130432
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 2122317824
+BOARD_CACHEIMAGE_PARTITION_SIZE := 318767104
 BOARD_FLASH_BLOCK_SIZE := 131072
-
-# Bluetooth
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/zte/msm8x30-common/bluetooth
-
-# Disable initlogo, ZTE framebuffer is weird
-TARGET_NO_INITLOGO := true
-
-# Use seperate speakerphone device
-BOARD_USES_SEPERATED_VOICE_SPEAKER := true
-
-# Use seperate devices for VOIP
-BOARD_USES_SEPERATED_VOIP := true
-
-# Use USB Dock Audio
-BOARD_HAVE_DOCK_USBAUDIO := true
-
-# Allow suspend in charge mode
-BOARD_CHARGER_ENABLE_SUSPEND := true
-
-# Enable QCOM FM feature
-QCOM_FM_ENABLED := true
-BOARD_USES_SEPERATED_FM := true
-
-# Camera
-TARGET_NEED_CAMERA_ZSL := true
-TARGET_NEED_FFC_PICTURE_FIXUP := true
-
-# We have new GPS
-BOARD_HAVE_NEW_QC_GPS := true
-
-
-# Time services
-BOARD_USES_QC_TIME_SERVICES := true
